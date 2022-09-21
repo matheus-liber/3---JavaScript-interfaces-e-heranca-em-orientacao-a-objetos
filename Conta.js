f70f530b9959d8fa4bb08ea2fd085aa6beeb600b
Matheus Silva <matheuslibergt@gmail.com>
2022-09-21 20:05:29 -0300

    Inserindo a classe Funcionário

--- Conta.js
+++ Conta.js
@@ -1,53 +1,44 @@
 export class Conta{
-    constructor(tipo, saldoInicial, cliente, agencia){
-        this._tipo = tipo;
+    constructor(saldoInicial, cliente, agencia){
         this._saldo = saldoInicial;
         this._cliente = cliente;
         this._agencia = agencia;
     }
-
-    set cliente(novoValor) {
-        if (novoValor instanceof Cliente) {
+    set cliente(novoValor){
+        if(novoValor instanceof Cliente){
             this._cliente = novoValor;
         }
     }
 
-    get cliente() {
+    get cliente(){
         return this._cliente;
     }
 
-    get saldo() {
+    get saldo(){
         return this._saldo;
     }
-
+    
     sacar(valor){
-        let = taxa = 1
-        if(this._tipo == "corrente"){
-            taxa = 1.1;
-        }
-        if(this._tipo == "salario"){
-            taxa = 1.05;
-        }
-        if(this._tipo == "empresarial"){
-            taxa = 1.15;
-        }
+        let taxa = 1;
+        return this._sacar(valor, taxa);
+    }
+
+    _sacar(valor, taxa){
         const valorSacado = taxa * valor;
-        if(this._saldo >= valor){
-            this._saldo -= valor;
+        if(this._saldo >= valorSacado){
+            this._saldo -= valorSacado;
             return valorSacado;
         }
+        return 0;
     }
 
     depositar(valor){
-        if(valor <= 0) return;
-        this._saldo += valor;   
+        this._saldo += valor;           
     }
 
-    transferir (valor, conta){
-        if(this._tipo == "salario"){
-            return;
-        }
+    tranferir(valor, conta){
         const valorSacado = this.sacar(valor);
         conta.depositar(valorSacado);
+        
     }
 }
